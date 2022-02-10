f92263f7a03f9e89f0f4912a6e1f8302410ade21
raziiramon <33711271+raziiramon@users.noreply.github.com>
2022-02-09 23:57:14 -0500

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -7,6 +7,31 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  /*declares the function */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if(solana && solana.isPhantom){
+        console.log('Phantom wallet found!');
+      } else{
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  /*when the function first mounts we check to see 
+  if the phantom wallet is connected */
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+
   return (
     <div className="App">
       <div className="container">
